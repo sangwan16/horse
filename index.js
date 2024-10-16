3d8c5a49dcbeb8637f38c0998e66ecfd6796094f
chauhan-ankit24 <91563712+chauhan-ankit24@users.noreply.github.com>
2024-10-16 22:54:01 +0530

    working

--- index.js
+++ index.js
@@ -1,36 +1,10 @@
-// // const express = require('express');
-// // const app = express();
-// const express = require('express');
-// const bodyParser = require('body-parser');
 
-// const app = express();
-// const port = 3000;
-
-// // Middleware to parse incoming request bodies
-// // app.use(bodyParser.json());
-
-// // Route to handle POST requests
-// // app.post('/', (req, res) => {
-// //   const data = req.body;
-// //   console.log('Received data:', data);
-// //   res.json({ message: 'Data received successfully!' });
-  
-// // });
-
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
 const express = require('express');
 const mongoose = require('mongoose');
-// require('dotenv').config(); // Optional if you're using environment variables
-
 const app = express();
 
-// MongoDB Atlas connection string (you can store it in environment variables for security)
-const mongoURI = "mongodb+srv://akashchauhan72520:Ankit2000@cluster0.qm7u4pa.mongodb.net/?retryWrites=true&w=majority";
+const mongoURI = "mongodb+srv://akash-mongo-db:randompage@cluster0.lj5iykk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-// Mongoose connection
 mongoose
   .connect(mongoURI)
   .then(() => {
@@ -43,10 +17,6 @@ mongoose
 // Middleware
 app.use(express.json());
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('MongoDB Atlas Connection Example');
-});
 
 // Start server
 const PORT = process.env.PORT || 3000;
